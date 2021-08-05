f796230cd93c7dfad2f20d0d05bcb24591b6a7e0
xialang <xialang@dtinsure.com>
2021-08-05 15:58:59 +0800

    jvm-自定义classloader

---
+++ geektime-jvm/src/main/java/jvm/HelloClassLoader.java
@@ -0,0 +1,53 @@
+package jvm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * @Description:
+ * @author: xialang
+ * @date 2021/8/5 11:55
+ */
+@Slf4j
+public class HelloClassLoader extends ClassLoader {
+
+    public static void main(String[] args) {
+        try {
+            Class<?> hello = new HelloClassLoader().loadClass("Hello");
+            for (Method m : hello.getDeclaredMethods()) {
+                System.out.println(hello.getSimpleName() + "." + m.getName());
+            }
+            Object instance = hello.newInstance();
+            Method method = hello.getMethod("hello");
+            method.invoke(instance);
+        } catch (ClassNotFoundException e) {
+            log.error("错误信息：", e);
+            e.printStackTrace();
+        } catch (IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) {
+        Class<?> aClass = null;
+        try {
+            File file = ResourceUtils.getFile("classpath:Hello.xlass");
+            byte[] bytes = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (255 - bytes[i]);
+            }
+            aClass = defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return aClass;
+    }
+}
