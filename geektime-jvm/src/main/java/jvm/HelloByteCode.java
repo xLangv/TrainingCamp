f96d9424f3f7d4909f4967c097a48e553ad34bec
xLangv <Xx@13297905870>
2021-08-14 11:41:07 +0800

    测试字节码

--- geektime-jvm/src/main/java/jvm/HelloByteCode.java
+++ geektime-jvm/src/main/java/jvm/HelloByteCode.java
@@ -1,10 +1,13 @@
 package jvm;
 
 public class HelloByteCode {
+    static Integer s = 2;
     public static void main(String[] args) {
         String s = "abc";
+        int dd = 100;
         int i = 1;
         i = i++;
+        System.out.println(2);
         HelloByteCode helloByteCode = new HelloByteCode();
     }
 }
